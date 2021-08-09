3e442310bb8419a6e01278eb62bbed10d417fdea
DelliB <56844702+DelliB@users.noreply.github.com>
2021-08-09 12:45:41 -0400

    Update MODIS_recovery.R

--- MODIS_recovery.R
+++ MODIS_recovery.R
@@ -16,9 +16,10 @@ data_directory <- '~/Documents/Kueppers lab'
 recovery <- read.csv(file.path(data_directory, "MODIS_recovery.csv"), stringsAsFactors= FALSE)
 
 # axis labels
-LAIreco = expression(Delta*"LAI Recovery [LAI/LAI]")
-EVIreco = expression(Delta*"EVI Recovery [EVI/EVI]")
-NDVIreco = expression(Delta*"NDVI Recovery [NDVI/NDVI]")
+LAIreco = expression(Delta*"LAI 500m [LAI/LAI]")
+EVIreco = expression(Delta*"EVI 250m [EVI/EVI]")
+NDVIreco = expression(Delta*"NDVI 250m [NDVI/NDVI]")
+x = "Time since disturbance (months)"
 
 ### Timeseries
 ## Pantropical data calculation
@@ -43,7 +44,7 @@ ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = reg
   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
               fill = "light blue", alpha = 0.05, color = NA) +
   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = "Time (months)", y = LAIreco, color = "Region") +
+  labs(x = x, y = LAIreco, color = "Region") +
   ylim(-1.5, 1.5) +
   geom_hline(yintercept=c(0), color = "gray") +
   theme_bw() +
@@ -58,6 +59,29 @@ ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = reg
         panel.grid.minor = element_blank()) +
   guides(color=guide_legend(nrow=2, byrow=TRUE))
 ggsave("recovery_LAI_500m.png", width = 7, height = 5, path = data_directory)
+
+LAI <- ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = region)) +
+  geom_line(data = LAIm[!is.na(LAIm$LAI_change_500m),]) +
+  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+              fill = "light blue", alpha = 0.05, color = NA) +
+  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+  labs(x = "", y = LAIreco, color = "Region") +
+  ylim(-1.5, 1.5) +
+  geom_hline(yintercept=c(0), color = "gray") +
+  theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = "top",
+        legend.title = element_text(size = 13, color = "black"),
+        legend.text = element_text(size = 13, color = "black"),
+        plot.title = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text = element_text(size = 13, color = "black"),
+        axis.title = element_text(size = 13, color = "black"),
+        panel.grid.minor = element_blank()) +
+  guides(color=guide_legend(nrow=2, byrow=TRUE))
+print(LAI)
 
 
 ## Pantropical data calculation
@@ -82,7 +106,7 @@ ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = reg
   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
               fill = "light blue", alpha = 0.05, color = NA) +
   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = "Time (months)", y = EVIreco, color = "Region") +
+  labs(x = x, y = EVIreco, color = "Region") +
   ylim(-1, 1) +
   geom_hline(yintercept=c(0), color = "gray") +
   theme_bw() +
@@ -97,6 +121,29 @@ ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = reg
         panel.grid.minor = element_blank()) +
   guides(color=guide_legend(nrow=2, byrow=TRUE))
 ggsave("recovery_EVI_250m.png", width = 7, height = 5, path = data_directory)
+
+EVI <- ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = region)) +
+  geom_line(data = EVIm[!is.na(EVIm$EVI_change_250m),]) +
+  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+              fill = "light blue", alpha = 0.05, color = NA) +
+  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+  labs(x = x, y = EVIreco, color = "Region") +
+  ylim(-.75, .75) +
+  geom_hline(yintercept=c(0), color = "gray") +
+  theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = "none",
+        #legend.title = element_text(size = 13, color = "black"),
+        #legend.text = element_text(size = 13, color = "black"),
+        plot.title = element_blank(),
+        #axis.text.x = element_blank(),
+        #axis.title.x = element_blank(),
+        axis.text = element_text(size = 13, color = "black"),
+        axis.title = element_text(size = 13, color = "black"),
+        panel.grid.minor = element_blank()) +
+  guides(color=guide_legend(nrow=2, byrow=TRUE))
+print(EVI)
 
 
 ## Pantropical data calculation
@@ -115,13 +162,13 @@ NDVIm$min = NDVIm$m - error
 NDVIm$max = NDVIm$m + error
 
 ## Plot
-# EVI
+# NDVI
 ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
   geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
               fill = "light blue", alpha = 0.05, color = NA) +
   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = "Time (months)", y = NDVIreco, color = "Region") +
+  labs(x = x, y = NDVIreco, color = "Region") +
   ylim(-1, 1) +
   geom_hline(yintercept=c(0), color = "gray") +
   theme_bw() +
@@ -136,4 +183,30 @@ ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = r
         panel.grid.minor = element_blank()) +
   guides(color=guide_legend(nrow=2, byrow=TRUE))
 ggsave("recovery_NDVI_250m.png", width = 7, height = 5, path = data_directory)
+
+NDVI <- ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
+  geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
+  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+              fill = "light blue", alpha = 0.05, color = NA) +
+  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+  labs(x = x, y = NDVIreco, color = "Region") +
+  ylim(-.6, .6) +
+  geom_hline(yintercept=c(0), color = "gray") +
+  theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = "none",
+        #legend.title = element_text(size = 13, color = "black"),
+        #legend.text = element_text(size = 13, color = "black"),
+        plot.title = element_blank(),
+        axis.text = element_text(size = 13, color = "black"),
+        axis.title = element_text(size = 13, color = "black"),
+        panel.grid.minor = element_blank()) +
+  guides(color=guide_legend(nrow=2, byrow=TRUE))
+print(NDVI)
+
+
+### Arranging plots together
+ggarrange(LAI, EVI, nrow = 2, labels = c("A", "B"))
+ggsave("MODIS_recovery.png", width = 7, height = 9, path = data_directory)
 
