bd18b2b9c3a9a84dd215c7346adb7dce3958eead
develyn.bloom <develyn.bloom@ufl.edu>
2022-01-27 14:25:57 -0500

    updates

--- MODIS_recovery.R
+++ MODIS_recovery.R
@@ -19,7 +19,7 @@ LFreco <- read.csv(file.path(data_directory, "MODIS_recovery_bb.csv"), stringsAs
 # axis labels
 LAIreco = expression(Delta*"LAI 500m [LAI/LAI]")
 EVIreco = expression(Delta*"EVI 250m [EVI/EVI]")
-NDVIreco = expression(Delta*"NDVI 250m [NDVI/NDVI]")
+#NDVIreco = expression(Delta*"NDVI 250m [NDVI/NDVI]")
 x = "Time since disturbance (months)"
 #Pantropical_LAI = c("Pantropical", expression(Delta*"LAI"))
 
@@ -48,7 +48,7 @@ LFreco$max = LFreco$Pantropical_Litterfall_Mean + (1.96*LFerror)
 
 ## Plot
 # LAI
-ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = region)) +
+LAI <- ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = region)) +
   geom_line(data = LAIm[!is.na(LAIm$LAI_change_500m),], linetype = "dashed") +
   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
               fill = "light blue", alpha = 0.05, color = NA) +
@@ -57,11 +57,19 @@ ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = reg
               fill = "gray", alpha = 0.25, color = NA) +
   geom_line(data = LFreco, aes(x = month, y = Pantropical_Litterfall_Mean, 
                                color = "Pantropical litterfall"), lwd=1) +
-  labs(x = x, y = LAIreco, color = "Region") +
+  labs(x = "", y = LAIreco, color = "Region") +
   ylim(-1.5, 1.5) +
   geom_hline(yintercept=c(0), color = "gray") +
-  scale_color_manual(values = c("red2", "yellow3", "springgreen4", "black", 
-                                "deepskyblue3", "orchid")) +
+  #scale_color_manual(values = c("red2", "yellow3", "springgreen4", "black", 
+   #                             "deepskyblue3", "orchid")) +
+  scale_colour_manual("Region", 
+                      breaks = c("Australia", "Caribbean", "Mexico", 
+                                 "Pantropical litterfall", "Pantropical Δ LAI", 
+                                 "Pantropical Δ EVI", "Taiwan"),
+                      values = c("Australia"="red2", "Caribbean"="yellow3", 
+                                 "Mexico"="springgreen4", "Pantropical litterfall"=
+                                   "black", "Pantropical Δ LAI"="deepskyblue3", 
+                                 "Pantropical Δ EVI"= "purple4", "Taiwan"="orchid")) +
   theme_bw() +
   theme(panel.background = element_blank(),
         panel.grid.major = element_blank(),
@@ -72,36 +80,36 @@ ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = reg
         axis.text = element_text(size = 13, color = "black"),
         axis.title = element_text(size = 13, color = "black"),
         panel.grid.minor = element_blank()) +
-  labs(caption = "Solid lines are pantropical mean and shaded areas represent 95% CI") +
-  guides(color = guide_legend(nrow=2,byrow=TRUE))
-ggsave("recovery_LAI_LF_500m.png", width = 7, height = 7, path = data_directory)
+  #labs(caption = "Solid lines are pantropical mean and shaded areas represent 95% CI") +
+  guides(color = guide_legend(ncol=4, byrow=FALSE))
+#ggsave("recovery_LAI_LF_500m.png", width = 7, height = 7, path = data_directory)
 
 #guides(color=guide_legend(nrow=2, byrow=TRUE)) +
 #  scale_color_manual(values = c( "Pantropical mean" = "light blue", "Pantropical LF mean" = "purple", 
  #                                "Australia" = "red", "Mexico" = "green", "Caribbean" = "tan", "Taiwan" = "pink"))
 
-LAI <- ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = region)) +
-  geom_line(data = LAIm[!is.na(LAIm$LAI_change_500m),]) +
-  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
-              fill = "light blue", alpha = 0.05, color = NA) +
-  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = "", y = LAIreco, color = "Region") +
-  ylim(-1.5, 1.5) +
-  geom_hline(yintercept=c(0), color = "gray") +
-  theme_bw() +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.position = "top",
-        legend.title = element_text(size = 13, color = "black"),
-        legend.text = element_text(size = 13, color = "black"),
-        plot.title = element_blank(),
-        axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
-        axis.text = element_text(size = 13, color = "black"),
-        axis.title = element_text(size = 13, color = "black"),
-        panel.grid.minor = element_blank()) +
-  guides(color=guide_legend(nrow=2, byrow=TRUE))
-print(LAI)
+# LAI <- ggplot(LAIm, aes(x = month, y = LAI_change_500m, group = case_study, color = region)) +
+#   geom_line(data = LAIm[!is.na(LAIm$LAI_change_500m),]) +
+#   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+#               fill = "light blue", alpha = 0.05, color = NA) +
+#   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+#   labs(x = "", y = LAIreco, color = "Region") +
+#   ylim(-1.5, 1.5) +
+#   geom_hline(yintercept=c(0), color = "gray") +
+#   theme_bw() +
+#   theme(panel.background = element_blank(),
+#         panel.grid.major = element_blank(),
+#         legend.position = "top",
+#         legend.title = element_text(size = 13, color = "black"),
+#         legend.text = element_text(size = 13, color = "black"),
+#         plot.title = element_blank(),
+#         axis.text.x = element_blank(),
+#         axis.title.x = element_blank(),
+#         axis.text = element_text(size = 13, color = "black"),
+#         axis.title = element_text(size = 13, color = "black"),
+#         panel.grid.minor = element_blank()) +
+#   guides(color=guide_legend(nrow=2, byrow=TRUE))
+# print(LAI)
 
 
 ## Pantropical data calculation
@@ -113,120 +121,137 @@ EVIm <- recovery %>%
             SD = sd(EVI_change_250m),
             n = length(EVI_change_250m))
 EVIm <- merge(EVIm, recovery, by = "month")
-error <- qnorm(0.975) * EVIm$SD/sqrt(EVIm$n)
+error <- EVIm$SD/sqrt(EVIm$n)
 # this is standard error
 # .975 if having a normal distribution and .95 if not?
-EVIm$min = EVIm$m - error
-EVIm$max = EVIm$m + error
+EVIm$min = EVIm$m - (1.96 * error)
+EVIm$max = EVIm$m + (1.96 * error)
 
 ## Plot
 # EVI
-ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = region)) +
-  geom_line(data = EVIm[!is.na(EVIm$EVI_change_250m),]) +
+EVI <- ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = region)) +
+  geom_line(data = EVIm[!is.na(EVIm$EVI_change_250m),], linetype = "dashed") +
   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
-              fill = "light blue", alpha = 0.05, color = NA) +
-  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+              fill = "purple1", alpha = 0.02, color = NA) +
+  geom_line(aes(x = month, y = m, color = "Pantropical Δ EVI"), lwd=1) +
+  geom_ribbon(data = LFreco, aes(x = month, ymin = CI_lower_bound, ymax = CI_upper_bound), 
+              fill = "gray", alpha = 0.25, color = NA) +
+  geom_line(data = LFreco, aes(x = month, y = Pantropical_Litterfall_Mean, 
+                               color = "Pantropical litterfall"), lwd=1) +
   labs(x = x, y = EVIreco, color = "Region") +
-  ylim(-1, 1) +
+  ylim(-1.5, 1) +
   geom_hline(yintercept=c(0), color = "gray") +
+  scale_color_manual(values = c("red2", "yellow3", "springgreen4", "black", 
+                               "purple4", "orchid")) +
   theme_bw() +
   theme(panel.background = element_blank(),
         panel.grid.major = element_blank(),
-        legend.position = "top",
+        legend.position = "none",
         legend.title = element_text(size = 13, color = "black"),
         legend.text = element_text(size = 13, color = "black"),
         plot.title = element_blank(),
         axis.text = element_text(size = 13, color = "black"),
         axis.title = element_text(size = 13, color = "black"),
         panel.grid.minor = element_blank()) +
-  guides(color=guide_legend(nrow=2, byrow=TRUE))
-ggsave("recovery_EVI_250m.png", width = 7, height = 5, path = data_directory)
+  labs(caption = "Solid lines are pantropical mean and shaded areas represent 95% CI") +
+  guides(color=guide_legend(ncol=1, byrow=FALSE))
+#ggsave("recovery_EVI_250m.png", width = 7, height = 5, path = data_directory)
 
-EVI <- ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = region)) +
-  geom_line(data = EVIm[!is.na(EVIm$EVI_change_250m),]) +
-  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
-              fill = "light blue", alpha = 0.05, color = NA) +
-  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = x, y = EVIreco, color = "Region") +
-  ylim(-.75, .75) +
-  geom_hline(yintercept=c(0), color = "gray") +
-  theme_bw() +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.position = "none",
-        #legend.title = element_text(size = 13, color = "black"),
-        #legend.text = element_text(size = 13, color = "black"),
-        plot.title = element_blank(),
-        #axis.text.x = element_blank(),
-        #axis.title.x = element_blank(),
-        axis.text = element_text(size = 13, color = "black"),
-        axis.title = element_text(size = 13, color = "black"),
-        panel.grid.minor = element_blank()) +
-  guides(color=guide_legend(nrow=2, byrow=TRUE))
-print(EVI)
+# Uniform legend
+# key <- scale_colour_manual("Region", 
+#                     breaks = c("Australia", "Caribbean", "Mexico", 
+#                                "Pantropical litterfall", "Pantropical Δ LAI", 
+#                                "Pantropical Δ EVI", "Taiwan"),
+#                     values = c("Australia"="red2", "Caribbean"="yellow3", 
+#                                "Mexico"="springgreen4", "Pantropical litterfall"=
+#                                  "black", "Pantropical Δ LAI"="deepskyblue3", 
+#                                "Pantropical Δ EVI"= "purple4", "Taiwan"="orchid"))
+
+# EVI <- ggplot(EVIm, aes(x = month, y = EVI_change_250m, group = case_study, color = region)) +
+#   geom_line(data = EVIm[!is.na(EVIm$EVI_change_250m),]) +
+#   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+#               fill = "light blue", alpha = 0.05, color = NA) +
+#   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+#   labs(x = x, y = EVIreco, color = "Region") +
+#   ylim(-.75, .75) +
+#   geom_hline(yintercept=c(0), color = "gray") +
+#   theme_bw() +
+#   theme(panel.background = element_blank(),
+#         panel.grid.major = element_blank(),
+#         legend.position = "none",
+#         #legend.title = element_text(size = 13, color = "black"),
+#         #legend.text = element_text(size = 13, color = "black"),
+#         plot.title = element_blank(),
+#         #axis.text.x = element_blank(),
+#         #axis.title.x = element_blank(),
+#         axis.text = element_text(size = 13, color = "black"),
+#         axis.title = element_text(size = 13, color = "black"),
+#         panel.grid.minor = element_blank()) +
+#   guides(color=guide_legend(nrow=2, byrow=TRUE))
+# print(EVI)
 
 
 ## Pantropical data calculation
 # EVI
-NDVIm <- recovery %>%
-  drop_na() %>%
-  group_by(month) %>%
-  summarize(m = mean(NDVI_change_250m),
-            SD = sd(NDVI_change_250m),
-            n = length(NDVI_change_250m))
-NDVIm <- merge(NDVIm, recovery, by = "month")
-error <- qnorm(0.975) * NDVIm$SD/sqrt(NDVIm$n)
-# this is standard error
-# .975 if having a normal distribution and .95 if not?
-NDVIm$min = NDVIm$m - error
-NDVIm$max = NDVIm$m + error
+# NDVIm <- recovery %>%
+#   drop_na() %>%
+#   group_by(month) %>%
+#   summarize(m = mean(NDVI_change_250m),
+#             SD = sd(NDVI_change_250m),
+#             n = length(NDVI_change_250m))
+# NDVIm <- merge(NDVIm, recovery, by = "month")
+# error <- qnorm(0.975) * NDVIm$SD/sqrt(NDVIm$n)
+# # this is standard error
+# # .975 if having a normal distribution and .95 if not?
+# NDVIm$min = NDVIm$m - error
+# NDVIm$max = NDVIm$m + error
 
 ## Plot
 # NDVI
-ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
-  geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
-  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
-              fill = "light blue", alpha = 0.05, color = NA) +
-  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = x, y = NDVIreco, color = "Region") +
-  ylim(-1, 1) +
-  geom_hline(yintercept=c(0), color = "gray") +
-  theme_bw() +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.position = "top",
-        legend.title = element_text(size = 13, color = "black"),
-        legend.text = element_text(size = 13, color = "black"),
-        plot.title = element_blank(),
-        axis.text = element_text(size = 13, color = "black"),
-        axis.title = element_text(size = 13, color = "black"),
-        panel.grid.minor = element_blank()) +
-  guides(color=guide_legend(nrow=2, byrow=TRUE))
-ggsave("recovery_NDVI_250m.png", width = 7, height = 5, path = data_directory)
-
-NDVI <- ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
-  geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
-  geom_ribbon(aes(x = month, ymin = min, ymax = max), 
-              fill = "light blue", alpha = 0.05, color = NA) +
-  geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
-  labs(x = x, y = NDVIreco, color = "Region") +
-  ylim(-.6, .6) +
-  geom_hline(yintercept=c(0), color = "gray") +
-  theme_bw() +
-  theme(panel.background = element_blank(),
-        panel.grid.major = element_blank(),
-        legend.position = "none",
-        #legend.title = element_text(size = 13, color = "black"),
-        #legend.text = element_text(size = 13, color = "black"),
-        plot.title = element_blank(),
-        axis.text = element_text(size = 13, color = "black"),
-        axis.title = element_text(size = 13, color = "black"),
-        panel.grid.minor = element_blank()) +
-  guides(color=guide_legend(nrow=2, byrow=TRUE))
-print(NDVI)
+# ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
+#   geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
+#   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+#               fill = "light blue", alpha = 0.05, color = NA) +
+#   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+#   labs(x = x, y = NDVIreco, color = "Region") +
+#   ylim(-1, 1) +
+#   geom_hline(yintercept=c(0), color = "gray") +
+#   theme_bw() +
+#   theme(panel.background = element_blank(),
+#         panel.grid.major = element_blank(),
+#         legend.position = "top",
+#         legend.title = element_text(size = 13, color = "black"),
+#         legend.text = element_text(size = 13, color = "black"),
+#         plot.title = element_blank(),
+#         axis.text = element_text(size = 13, color = "black"),
+#         axis.title = element_text(size = 13, color = "black"),
+#         panel.grid.minor = element_blank()) +
+#   guides(color=guide_legend(nrow=2, byrow=TRUE))
+# ggsave("recovery_NDVI_250m.png", width = 7, height = 5, path = data_directory)
+# 
+# NDVI <- ggplot(NDVIm, aes(x = month, y = NDVI_change_250m, group = case_study, color = region)) +
+#   geom_line(data = NDVIm[!is.na(NDVIm$NDVI_change_250m),]) +
+#   geom_ribbon(aes(x = month, ymin = min, ymax = max), 
+#               fill = "light blue", alpha = 0.05, color = NA) +
+#   geom_line(aes(x = month, y = m, color = "Pantropical mean")) +
+#   labs(x = x, y = NDVIreco, color = "Region") +
+#   ylim(-.6, .6) +
+#   geom_hline(yintercept=c(0), color = "gray") +
+#   theme_bw() +
+#   theme(panel.background = element_blank(),
+#         panel.grid.major = element_blank(),
+#         legend.position = "none",
+#         #legend.title = element_text(size = 13, color = "black"),
+#         #legend.text = element_text(size = 13, color = "black"),
+#         plot.title = element_blank(),
+#         axis.text = element_text(size = 13, color = "black"),
+#         axis.title = element_text(size = 13, color = "black"),
+#         panel.grid.minor = element_blank()) +
+#   guides(color=guide_legend(nrow=2, byrow=TRUE))
+# print(NDVI)
 
 
 ### Arranging plots together
 ggarrange(LAI, EVI, nrow = 2, labels = c("A", "B"))
-ggsave("MODIS_recovery.png", width = 7, height = 9, path = data_directory)
+ggsave("MODIS_recovery.png", width = 8, height = 9, path = data_directory)
 
--- PCA_trial.R
+++ PCA_trial.R
@@ -6,6 +6,9 @@
 library(tidyverse)
 library(factoextra)
 library(ggbiplot)
+library(ggcorrplot)
+library(ggpubr)
+library(multcompView)
 
 # Dellena file path
 #data_directory1 <- '~/Documents/GitHub/NGEEtropics_SULI_spring'
@@ -22,6 +25,10 @@ VI$change_annual_total_litterfall = log(VI$Response_Post_Mean_Tot_Litterfall
 
 # select columns for analysis
 names(VI)
+
+
+
+# LAI
 #VI.PCA <- VI %>% select() # another way to select the columns
 VI.PCA <- VI[,c("change_annual_LAI_500m", "Holdridge_ID", "soil_P", "Longitude", "gale.wind.duration..minutes.",
                 "peak_wind_speed_ms", "change_annual_total_litterfall", 
@@ -88,9 +95,9 @@ fviz_pca_biplot(VI.pca, repel = TRUE,
 )
 
 # to make plot with color
-country <- c("Australia", rep("Taiwan", 4), rep("Caribbean", 4), rep("Taiwan", 4), rep("Caribbean", 2), rep("Mexico", 2), rep("Caribbean", 2))
+country <- c(rep("Caribbean", 2), rep("Mexico", 2), rep("Caribbean", 4), rep("Taiwan", 8), rep("Caribbean", 2), "Australia")
 print(country)
-ggbiplot(VI.pca, ellipse = TRUE, obs.scale = 1, var.scale = 1, groups = country) +
+LAI <- ggbiplot(VI.pca, ellipse = TRUE, obs.scale = 1, var.scale = 1, groups = country) +
   labs(color = "Region") +
   ylim(-4.8, 4.1) +
   xlim(-4.2, 5.5) +
@@ -99,56 +106,97 @@ ggbiplot(VI.pca, ellipse = TRUE, obs.scale = 1, var.scale = 1, groups = country)
         panel.grid.major = element_blank(),
         axis.title = element_text(size = 13, color = "black"),
         axis.text = element_text(size = 12, color = "black"),
-        legend.position = "bottom",
+        legend.position = "none",
         legend.text = element_text(size = 13, color = "black"),
         legend.title = element_text(size = 13, color = "black"))
 ggsave("PCA.png", width = 7, height = 6, path = data_directory)
 
 
 
+# EVI
+VI.PCA <- VI[,c("change_annual_EVI_250m", "Holdridge_ID", "soil_P", "Longitude", "gale.wind.duration..minutes.",
+                "peak_wind_speed_ms", "change_annual_total_litterfall", 
+                "Years_since_last_storm", "Elevation_m", "Cyclone_frequency", "Parent_material", "MAT_MAP_ratio_X100",
+                "Cyclone_Rainfall_mm", "Region", "N")]
 
-# Barbara's code
-#packages
-library(devtools)
-install_github("vqv/ggbiplot")
-library(grid)
+# drop NAs
+VI.PCA <- VI.PCA %>%
+  drop_na()
 
-#reading data
-PCA_soil <- VI2.PCA.B#PCA_soil_updated (including averages 0-50 cm)
-names(PCA_soil)
-str(PCA_soil)#n=45 of 23 variables
+# write csv to add ID columns for non-numeric variables
+write.csv(VI.PCA, file = "~/Documents/Kueppers Lab/case_study_for_PCA.csv")
 
-#changing data types
-PCA_soil$FT=as.factor(PCA_soil$FT)
-PCA_soil$Plot=as.factor(PCA_soil$Plot)
-PCA_soil$Depth=as.factor(PCA_soil$Depth)
-head(PCA_soil)
+# get number of unique variable names for non-numeric categories
+unique(levels(as.factor(VI.PCA$Parent_material)))
+#unique(levels(as.factor(VI.PCA$Cyclone_name)))
 
-#setting up dataframe with the desired columns from original dataset
-biplot2<-PCA_soil[,c(1:14)]
+# read in new csv
+VI2 <- read.csv(file.path(data_directory, "case_study_for_PCA.csv"), na.strings = "NA")
 
-names(biplot2)=c("change_annual_LAI_500m", "Holdridge_ID", "soil_P", "Longitude", "gale.wind.duration..minutes.",
-                 "peak_wind_speed_ms", "change_annual_total_litterfall", "change_subannual_total_litterfall",
-                 "Years_since_last_storm", "Elevation_m", "Cyclone_frequency", "Parent_material_ID", "MAT_MAP_ratio_X100",
-                 "Cyclone_Rainfall_mm")
-names(biplot2)
+# select only numeric columns and remove parent materials
+VI2.PCA <- VI2[,c("change_annual_EVI_250m", "Holdridge_ID", "soil_P", "Longitude", "gale.wind.duration..minutes.",
+                  "peak_wind_speed_ms", "change_annual_total_litterfall",
+                  "Years_since_last_storm", "Elevation_m", "Cyclone_frequency", "Parent_material_ID", "MAT_MAP_ratio_X100",
+                  "Cyclone_Rainfall_mm")]
+# maybe include a few other VIs and see if its related to the resolution or 
+# if its due to the VI
 
-#creating vector with the code I desired to color the points with
-#you can do the same for region instead of the numbers within " "
-yearspl2<-c(rep("Caribbean",8),rep("Mexico", 2), rep("Taiwan",4))
-yearspl2
+# rename columns
+VI2.PCA <- dplyr::rename(VI2.PCA, "change annual EVI (250m)" = change_annual_EVI_250m)
+VI2.PCA <- dplyr::rename(VI2.PCA, "Holdridge Life Zone" = Holdridge_ID)
+VI2.PCA <- dplyr::rename(VI2.PCA, "soil phosphorus" = soil_P)
+VI2.PCA <- dplyr::rename(VI2.PCA, "wind duration" = gale.wind.duration..minutes.)
+VI2.PCA <- dplyr::rename(VI2.PCA, "wind speed" = peak_wind_speed_ms)
+VI2.PCA <- dplyr::rename(VI2.PCA, "change TL" = change_annual_total_litterfall)
+VI2.PCA <- dplyr::rename(VI2.PCA, "years since last storm" = Years_since_last_storm)
+VI2.PCA <- dplyr::rename(VI2.PCA, "elevation" = Elevation_m)
+VI2.PCA <- dplyr::rename(VI2.PCA, "cyclone frequency" = Cyclone_frequency)
+VI2.PCA <- dplyr::rename(VI2.PCA, "parent material" = Parent_material_ID)
+VI2.PCA <- dplyr::rename(VI2.PCA, "MAT:MAP ratio" = MAT_MAP_ratio_X100)
+VI2.PCA <- dplyr::rename(VI2.PCA, "cyclone rainfall" = Cyclone_Rainfall_mm)
 
-#running the pca
-pca_soil <- prcomp(biplot2, center = TRUE,scale. = TRUE)#standardized data
-summary(pca_soil)
-str(pca_soil)
-pca_soil$rotation
+# select for non-NA values
+VI2.PCA.B <- VI2.PCA %>%
+  drop_na()
 
-#ploting the pca biplot
-ggbiplot(pca_soil, varname.size=6.5,ellipse=TRUE,varname.abbrev=FALSE,labels.size = 7,groups=yearspl2,obs.scale=1)+theme_minimal()+theme(legend.position = "top")+       # Thicker line
-  geom_point(aes(shape = yearspl2,color=yearspl2),         # Hollow squares
-             size = 5.5)+
-  scale_shape(solid=FALSE)+theme(axis.text=element_text(size=16),axis.title=element_text(size=16))+labs(x="PC1 (48%)",y="PC2 (14%)")
+# compute PCA
+VI.pca <- prcomp(VI2.PCA.B, scale = TRUE)
 
+# visualize PCA components with Scree plot
+fviz_eig(VI.pca)
+
+# visualize PCA with PCA plot
+fviz_pca_var(VI.pca,
+             #col.var = "contrib", # Color by contributions to the PC
+             #gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+# visualize PCA with biplot
+fviz_pca_biplot(VI.pca, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+# to make plot with color
+country <- c(rep("Caribbean", 2), rep("Mexico", 2), rep("Caribbean", 4), rep("Taiwan", 8), rep("Caribbean", 2), "Australia")
+print(country)
+EVI <- ggbiplot(VI.pca, ellipse = TRUE, obs.scale = 1, var.scale = 1, groups = country) +
+  labs(color = "Region") +
+  ylim(-4.8, 4.1) +
+  xlim(-4.2, 5.5) +
+  theme_bw() +
+  theme(panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        axis.title = element_text(size = 13, color = "black"),
+        axis.text = element_text(size = 12, color = "black"),
+        legend.position = "right",
+        legend.text = element_text(size = 13, color = "black"),
+        legend.title = element_text(size = 13, color = "black"))
+ggsave("PCA.png", width = 7, height = 6, path = data_directory)
+
+
+ggarrange(LAI, EVI, ncol = 2, labels = c("A", "B"))
+ggsave("PCA_analysis.png", width = 10, height = 9, path = data_directory)
 
 
