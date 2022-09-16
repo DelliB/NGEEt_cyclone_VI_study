ad0248073b09443e370f3cf3b8189cd0abb0bf97
Dellena Bloom <develyn.bloom@ufl.edu>
2022-09-16 12:58:18 -0400

    adding

--- stats/Bloom_NGEEtropics_paper_stats.R
+++ stats/Bloom_NGEEtropics_paper_stats.R
@@ -27,7 +27,7 @@ VIs_sites <- read.csv(file.path(data_directory, "case_study_data.csv"),
 ### Stats
 ## Regressions
 # LAI vs log transformed soil P
-LAIsoilP <- lm(change_annual_LAI_500m ~ log(soil_P), data = VIs_sites)
+LAIsoilP <- lm(change_annual_LAI_500m ~ log10(soil_P), data = VIs_sites)
 # Print summary of regression in console
 summary(LAIsoilP)
 # Save regression output
@@ -36,7 +36,7 @@ tab_model(LAIsoilP, file = "LAI_soilP_Reg.html")
 webshot("LAI_soilP_Reg.html", "LAI_soilP_Reg.png")
 
 # EVI vs log transformed soil P
-EVIsoilP <- lm(change_annual_EVI_250m ~ log(soil_P), data = VIs_sites)
+EVIsoilP <- lm(change_annual_EVI_250m ~ log10(soil_P), data = VIs_sites)
 summary(EVIsoilP)
 tab_model(EVIsoilP, file = "EVI_soilP_Reg.html")
 webshot("EVI_soilP_Reg.html", "EVI_soilP_Reg.png")
@@ -45,14 +45,14 @@ webshot("EVI_soilP_Reg.html", "EVI_soilP_Reg.png")
 ## Mixed effects
 # Fixed effects: LAI vs log transformed soil phosphorous
 # Mixed effects: Wind speed effects on 
-LAIsoilPWS <- lmer(change_annual_LAI_500m ~ log(soil_P) + (1|peak_wind_speed_ms), 
+LAIsoilPWS <- lmer(change_annual_LAI_500m ~ log10(soil_P) + (1|peak_wind_speed_ms), 
                    data = VIs_sites)
 summary(LAIsoilPWS)
 tab_model(LAIsoilPWS, file = "LAI_soilP_wind_ME.html")
 webshot("LAI_soilP_wind_ME.html", "LAI_soilP_wind_ME.png")
 
 # Wind speed effects on EVI vs log transformed soil phosphorous regression
-EVIsoilPWS <- lmer(change_annual_EVI_250m ~ log(soil_P) + (1|peak_wind_speed_ms), 
+EVIsoilPWS <- lmer(change_annual_EVI_250m ~ log10(soil_P) + (1|peak_wind_speed_ms), 
                    data = VIs_sites)
 summary(EVIsoilPWS)
 tab_model(EVIsoilPWS, file = "EVI_soilP_wind_ME.html")
